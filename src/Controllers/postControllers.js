9c96a832d21c1a7549dd0766987a8b425f1665dc
hoangnghiahp0107 <hoangnghiahp0107@gmail.com>
2024-05-18 13:52:10 +0700

    day1:s

--- src/Controllers/postControllers.js
+++ src/Controllers/postControllers.js
@@ -2,14 +2,18 @@ import sequelize from "../Models/index.js";
 import initModels from "../Models/init-models.js";
 const model = initModels(sequelize);
 
-const getPost = async(req, res) =>{
-    try{
-        const data = await model.BaiViet.findAll();
+const getPost = async(req, res) => {
+    try {
+        const data = await model.BaiViet.findAll({
+            where: {
+                CheDoRiengTuID: 1
+            }
+        });
         res.send(data);
-    }catch(error){
+    } catch(error) {
         console.log(error);
         res.status(500).send("Lỗi khi lấy dữ liệu");
     }
 }
 
-export {getPost}
+export { getPost };
